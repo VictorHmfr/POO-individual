a455362cbebeed86ddd6873ac1dcbd5d0dad127e
VictorHmfr <vhmartinsfranca@gmail.com>
2024-04-16 23:15:21 -0300

    feat: menu

---
+++ src/br/com/poo/pooindividual/application/Application.java
@@ -0,0 +1,28 @@
+package br.com.poo.pooindividual.application;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import br.com.poo.pooindividual.io.RelatorioIO;
+import br.com.poo.pooindividual.services.FuncionarioService;
+import br.com.poo.pooindividual.services.MenuService;
+
+public class Application {
+
+	//public static Scanner sc = new Scanner(System.in);
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+		//FuncionarioService funcService = new FuncionarioService();
+		
+		RelatorioIO.leitor("banco");
+		MenuService.menu();
+		//RelatorioIO.escritor("banco");
+		//RelatorioIO.relatorioListaDepartamentos(null));
+		//RelatorioIO.relatorioListaFuncionario(funcService.listaNomesFuncionarios());
+		//sc.close();
+		
+	}
+
+}
