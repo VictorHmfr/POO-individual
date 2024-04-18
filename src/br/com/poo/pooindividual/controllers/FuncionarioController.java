937868c65d4f60d98c01827f555b56d0ce05a20e
VictorHmfr <vhmartinsfranca@gmail.com>
2024-04-18 17:31:45 -0300

    feat: adicionando método de impressão individual

--- src/br/com/poo/pooindividual/controllers/FuncionarioController.java
+++ src/br/com/poo/pooindividual/controllers/FuncionarioController.java
@@ -9,36 +9,44 @@ import br.com.poo.pooindividual.services.FuncionarioService;
 import br.com.poo.pooindividual.util.Util;
 
 public class FuncionarioController {
-	
-	//instância do serviço do Funcionário, construção do objeto funcionarioService
+
+	// instância do serviço do Funcionário, construção do objeto funcionarioService
 	FuncionarioService funcService = new FuncionarioService();
 	static Logger logger = Util.setupLogger();
-	
+
 	public void listarNomesFuncionarios() throws IOException {
-		
-		//Acessa o método listaNomesFuncionarios da classe FuncionarioService
+
+		// Acessa o método listaNomesFuncionarios da classe FuncionarioService
 		List<String> funcionarios = funcService.listaNomesFuncionarios();
 		Util.customizer();
-		logger.log(Level.INFO, funcionarios + "\n\nLista gerada com sucesso!");
-		
-		
-		
+		logger.log(Level.INFO, "\n\nLista gerada com sucesso!");
+
 	}
-	
+
 	public void listarFuncionarioDepartamento() throws IOException {
-		
+
 		funcService.listaFuncionarioDepartamento();
 		Util.customizer();
 		logger.log(Level.INFO, "\n\nLista gerada com sucesso!");
-		
+
 	}
 
 	public void listarNomesFuncionariosCons() throws IOException {
-		
+
 		List<String> funcionariosConsole = funcService.listaNomesFuncionariosCons();
 		Util.customizer();
-		logger.log(Level.INFO, () -> funcionariosConsole.toString().replace("[", " ").replace("]", "").replace(",", "\n\n") + "\n\nLista gerada com sucesso!\n");
-		
+		logger.log(Level.INFO,
+				() -> funcionariosConsole.toString().replace("[", " ").replace("]", "").replace(",", "\n\n")
+						+ "\n\nLista gerada com sucesso!\n");
+
+	}
+
+	public void listarFuncionarioDepartamentoCons() throws IOException {
+
+		funcService.listaFuncionarioDepartamentoCons();
+		Util.customizer();
+		logger.log(Level.INFO, "\n\nLista gerada com sucesso!");
+
 	}
 	
 }
--- src/br/com/poo/pooindividual/services/FuncionarioService.java
+++ src/br/com/poo/pooindividual/services/FuncionarioService.java
@@ -3,58 +3,89 @@ package br.com.poo.pooindividual.services;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import br.com.poo.pooindividual.entities.Departamento;
 import br.com.poo.pooindividual.entities.Funcionario;
 import br.com.poo.pooindividual.io.RelatorioIO;
+import br.com.poo.pooindividual.util.Util;
 
 public class FuncionarioService {
-
 	
-	//Método que retorna uma lista do tipo String
+	static Logger logger = Util.setupLogger();
+
+	// Método que retorna uma lista do tipo String
 	public List<String> listaNomesFuncionarios() throws IOException {
-		//Cria uma instância de lsita vazia do tipo String
+		// Cria uma instância de lsita vazia do tipo String
 		List<String> listaNomesFuncionarios = new ArrayList<>();
-		//Cria um foreach para percorrer o mapa de Pets 
-		for(Funcionario func : Funcionario.getMapaFuncionarios().values()) {
-			//Adiciona o nome do pet na lista listaNomesFuncionarios
+		// Cria um foreach para percorrer o mapa de Pets
+		for (Funcionario func : Funcionario.getMapaFuncionarios().values()) {
+			// Adiciona o nome do pet na lista listaNomesFuncionarios
 			listaNomesFuncionarios.add(func.getNome());
 		}
 		RelatorioIO.relatorioListaFuncionario(listaNomesFuncionarios);
-		//Retorna a lista listaNomesFuncionarios com todos os nomes dos funcionarios
+		// Retorna a lista listaNomesFuncionarios com todos os nomes dos funcionarios
 		return listaNomesFuncionarios;
 	}
-	
+
 	public void listaFuncionarioDepartamento() throws IOException {
 		DepartamentoService departamentoService = new DepartamentoService();
-		RelatorioIO.relatorioListaFuncionarioDepartamento(listaFuncionarios(), departamentoService.listaDepartamentos());
+		RelatorioIO.relatorioListaFuncionarioDepartamento(listaFuncionarios(),
+				departamentoService.listaDepartamentos());
 	}
-	
-	//Método que retorna uma lista do tipo String
-		public List<Funcionario> listaFuncionarios() throws IOException {
-			//Cria uma instância de lsita vazia do tipo String
-			List<Funcionario> listaFuncionarios = new ArrayList<>();
-			//Cria um foreach para percorrer o mapa de Pets 
-			for(Funcionario func : Funcionario.getMapaFuncionarios().values()) {
-				//Adiciona o nome do pet na lista listaNomesFuncionarios
-				listaFuncionarios.add(func);
-			}
-			
-			//Retorna a lista listaNomesFuncionarios com todos os nomes dos funcionarios
-			return listaFuncionarios;
+
+	// Método que retorna uma lista do tipo String
+	public List<Funcionario> listaFuncionarios() throws IOException {
+		// Cria uma instância de lsita vazia do tipo String
+		List<Funcionario> listaFuncionarios = new ArrayList<>();
+		// Cria um foreach para percorrer o mapa de Pets
+		for (Funcionario func : Funcionario.getMapaFuncionarios().values()) {
+			// Adiciona o nome do pet na lista listaNomesFuncionarios
+			listaFuncionarios.add(func);
 		}
-	
-	
-		public List<String> listaNomesFuncionariosCons() throws IOException {
-		
-			List<String> listaNomesFuncionariosCons = new ArrayList<>();
-		
-			for(Funcionario func : Funcionario.getMapaFuncionarios().values()) {
-				
-				listaNomesFuncionariosCons.add(func.getNome());
-			}
-			
-			return listaNomesFuncionariosCons;
+
+		// Retorna a lista listaNomesFuncionarios com todos os nomes dos funcionarios
+		return listaFuncionarios;
+	}
+
+	public List<String> listaNomesFuncionariosCons() throws IOException {
+
+		List<String> listaNomesFuncionariosCons = new ArrayList<>();
+
+		for (Funcionario func : Funcionario.getMapaFuncionarios().values()) {
+
+			listaNomesFuncionariosCons.add(func.getNome());
 		}
+
+		return listaNomesFuncionariosCons;
+	}
+
+	public List<String> listaFuncionarioDepartamentoCons() throws IOException {
 		
-		
+        List<String> listaFuncionarioDepartamentoCons = new ArrayList<>();
+        Map<Integer, Departamento> deps = Departamento.getMapaDepartamentos();
+        for (Map.Entry<Integer, Funcionario> setFuncionario :
+            Funcionario.getMapaFuncionarios().entrySet()) {
+
+
+           logger.log(Level.INFO, setFuncionario.getValue().getNome() + " = "
+                              + deps.get(setFuncionario.getValue().getFkDep()).getNome());
+       }
+        /*for (Funcionario func : Funcionario.getMapaFuncionarios().values()) {
+            for (Departamento dep : Departamento.getMapaDepartamentos().values()) {
+                if (func.getFkDep() == dep.getId()) {
+                    listaFuncionarioDepartamentoCons.add(func.getNome());
+
+                }
+            }
+        }
+        */
+        //DepartamentoService departamentoService = new DepartamentoService();
+       return listaFuncionarioDepartamentoCons;
+
+    }
+	
+	
 }
